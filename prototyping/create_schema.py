7cb28baaabfbeda39f0afdc76a93579e4fa3d14e
James Munns <james.munns@gmail.com>
2015-10-27 22:09:39 +0100

    Initial testing, works to populate data in my demo environment

---
+++ prototyping/create_schema.py
@@ -0,0 +1,31 @@
+import psycopg2
+import getpass
+
+username = getpass.getuser()
+password = getpass.getpass("Database password for {}: ".format(username))
+database = "wate"
+
+def create_user_table(cursor):
+  user_schema = """
+    CREATE TABLE users (
+      id serial PRIMARY KEY,
+      name text NOT NULL,
+      email citext UNIQUE NOT NULL,
+      joindate date NOT NULL);
+    """
+  cursor.execute(user_schema)
+
+def create_weight_table(cursor):
+  weight_schema = """
+    CREATE TABLE weights (
+      user_id integer REFERENCES users(id) NOT NULL,
+      weight_lbs numeric CHECK (weight_lbs > 0) NOT NULL,
+      measure_date date NOT NULL,
+      measure_time time);
+    """
+  cursor.execute(weight_schema)  
+
+with psycopg2.connect(dbname=database, user=username, password=password) as conn:
+  with conn.cursor() as cur:
+    create_user_table(cur)
+    create_weight_table(cur)
---
+++ prototyping/fill_fake_data.py
@@ -0,0 +1,67 @@
+from faker import Factory
+from random import random, randint
+from datetime import date, timedelta
+import psycopg2
+import getpass
+from decimal import Decimal
+
+ff = Factory.create()
+
+def generate_users(cursor):
+  min_users = 3
+  max_users = 10
+
+  num_users = randint(min_users, max_users)
+
+  for _ in xrange(num_users):
+    name  = ff.name()
+    email = ff.email()
+    jdate = date.today() - timedelta(days=randint(5,100))
+
+    # print name, email, jdate,
+
+    min_sample = 5
+    max_sample = 10
+    num_sample = randint( min_sample, max_sample )
+
+    user_cmd = """
+      INSERT INTO users ( name, email, joindate )
+      VALUES ( %s, %s, %s );
+      """
+
+    cursor.execute( user_cmd, [ name, email, jdate ] )
+
+    # Get userid
+    id_fetch_cmd = """
+      SELECT * from users where email = %s;
+      """
+
+    cursor.execute( id_fetch_cmd, [ email ] )
+    users = cursor.fetchall()
+    assert len( users ) == 1
+    userid = users[0][0]
+
+    # Weights for the users
+    max_wgt = float(randint( 120, 200 ))
+    min_wgt = float(randint( 120, max_wgt ))
+    
+    # weight cmd
+    wgt_cmd = """
+      INSERT INTO weights ( user_id, weight_lbs, measure_date )
+      VALUES ( %s, %s, %s );
+      """
+
+    for samp in xrange(num_sample):
+      weight = float(max_wgt) - ( ( max_wgt - min_wgt ) * ( float(samp) / num_sample ) + ( 2 * random() ) )
+      weight = Decimal( "%.2f" % weight )
+      wdate  = jdate + timedelta(days=samp)
+      # print wdate, weight, " > "
+      cursor.execute( wgt_cmd, [ userid, weight, wdate ] )      
+
+    # print ""
+
+db = "wate"
+
+with psycopg2.connect( dbname=db, user=getpass.getuser(), password=getpass.getpass("Password for user {}: ".format(getpass.getuser()))) as conn:
+  with conn.cursor() as cur:
+    generate_users(cur)
